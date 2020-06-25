2c877caf4ad702573afd433221e060bd42f5ac3e
unknown <ZamoraLeo>
2020-06-24 19:49:43 -0500

    Agregado el examen final

--- Examen 3/js/agregar.js
+++ Examen 3/js/agregar.js
@@ -1,5 +1,18 @@
 const db = firebase.firestore();
 const storage = firebase.storage();
+const btnLogout = document.getElementById('btnLogout');
+    btnLogout.addEventListener('click', e => {
+        firebase.auth().signOut();
+     });
+     const auth = firebase.auth();
+     auth.onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser);
+        } else {
+            console.log('No Logueado')
+            window.location.href = "login.html"
+        }
+    })
 
 const txtNombre = document.getElementById('txtNombre');
 const txtDescripcion = document.getElementById('txtDescripcion');
